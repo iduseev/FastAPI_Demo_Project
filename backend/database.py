1dd2af53b50ad21e141f26add6e419f581942fb6
Ilgiz Duseev <ilgiz.duseev@hotmail.com>
2023-04-07 22:45:11 +0600

    Added methods to database module

--- backend/database.py
+++ backend/database.py
@@ -52,7 +52,15 @@ class MongoAdapter:
 
     def __str__(self):
         return """
-            Some info
+            The MongoAdapter class is used to control and support the work with MongoDB collections executed for 
+            FastAPI Demo Project service purposes.
+            Supports either usage with the MongoDB that requires authentication (accepts username, password, 
+            auth_source, auth_mechanism if bool flag 'requires_auth' passed as True).
+            Accepts passing custom 'required_index_params' in the proper format to ensure that important indexes are set 
+            for the given collection (passes <[("book_id", True)]> as 'required_index_params' by default).
+            Automatically calls inner method 'recreate_required_indexes' to ensure that all needed indexes will be set.
+            Allows turning off automatic indexes recreation by manual passing False to the bool flag 
+            'recreate_indexes' (True by default).
         """
     
     def __repr__(self):
@@ -84,4 +92,132 @@ class MongoAdapter:
                 port=self.port
             )
 
-    
+    def recreate_required_indexes(self) -> None:
+        """
+        Checks that required index names exist within the collection, restores them if they does not exist yet,
+        and marks selected indexes as either unique to allow only unique entries in DB or not unique
+        (depending on developer choice)
+
+        :param required_index_params: list of tuples of the following format:
+                                    [("<index_name>", <uniqueness_bool>), ("<index_name>", <uniqueness_bool>)],
+                                    where <index_name> indicates required index name, while <uniqueness_bool> flag
+                                    marks if required index should be unique
+        :type required_index_params: List[Tuple[str, bool]]
+        :return None:
+        """
+        # extract currently existing indexes from the DB collection
+        db_index_names_tuple = tuple(dict(self.collection.index_information()).keys())
+        if self.logger: self.logger.debug(
+            f"There are the following indexes {self.collection.name} within the collection: {', '.join(db_index_names_tuple)}"
+        )
+        # check that self.required_index_params value is valid, else return None
+        if not isinstance(self.required_index_params, list):
+            raise ValueError(
+                f"Invalid value was passed as <required_index_params> or the format is not compatible with the requirements: "
+                f"List[Tuple[str, bool]]"
+            )
+        # iterate over List[Tuple[str, bool]]
+        for required_index_data_piece in self.required_index_params:
+            if not isinstance(required_index_data_piece, tuple) or len(required_index_data_piece) != 2:
+                raise ValueError(
+                    f"One of the elements of <required_index_params> has invalid value or the format is not compatible with the requirements: "
+                    f"Tuple[str, bool], or there were more than 2 elements passed within the tuple"
+                )
+            required_index_name, uniqueness_bool = required_index_data_piece
+            # check if need to restore absent index name
+            if required_index_name not in db_index_names_tuple:
+                if self.logger: self.logger.warning(
+                    f"THe following index is required for the work: {required_index_name}, although it is absent in the collection. "
+                    f"Required index will be restored!"
+                )
+                self.collection.create_index(
+                    [(required_index_name, pymongo.ASCENDING)],
+                    unique=uniqueness_bool,
+                    name=required_index_name
+                )
+        
+    def insert_db_entry(self, data: Dict[str, Any]) -> Any:
+            """
+            Writes document in the collection, returning _id of inserted document
+
+            :param Dict[str, Any] data: document to be written in DB
+            :return Any: _id of inserted entry
+            """
+            return self.collection.insert_one(data).inserted_id
+
+    def silent_replace_db_entry(self, index_name: str, data: Dict[str, Any]) -> Any:
+        """
+        Silently replaces (updates and inserts) DB entry with new data piece by given index name.
+        If no DB entry is found by the filter within the collection, creates DB entry with new data piece without
+        raising exception.
+
+        :param str index_name: index name used to filter data in DB collection
+        :param Dict[str, Any] data: new data piece
+        :raises ValueError: if number of modified entries in DB collection is not equal to 1, raises ValueError
+        :return Any upserted_id: the _id of the new document if a matching document did not exist and
+                                inserting a new document took place. Otherwise None
+        """
+        q_filter = {index_name: data[index_name]}
+        result = self.collection.replace_one(filter=q_filter, replacement=data, upsert=True)
+        if result.modified_count > 1:
+            raise ValueError(
+                f"Error while rewriting the block of data! Replaced entries: {result.modified_count}, expected: 1!"
+            )
+        return result.upserted_id
+
+    def extract_db_entry(self, index_name: str, entry_id: str) -> Any:
+        """
+        Extract one document from the DB collection using filter based on passed index_name and entry_id
+
+        :param str index_name: index name used to filter data in DB collection
+        :param Any entry_id: ID of the DB entry to be extracted from the collection
+        :return Any: document
+        """
+        q_filter = {index_name: entry_id}
+        result = self.collection.find_one(filter=q_filter)
+        return result
+
+    def read_first_match(self, data: Dict[str, Any]) -> Dict[str, Any]:
+        """
+        Returns first matching document from the collection
+
+        :param Dict[str, Any] data: document to be written in DB
+        :return Dict[str: Any]: document
+        """
+        return self.collection.find_one(data)
+
+    def get_collection_names(self) -> List[AnyStr]:
+        """
+        Gets a list of all the collection names in this database
+
+        :return List[AnyStr]: list of all the collection names in the DB
+        """
+        return self.db.list_collection_names()
+
+    def delete_db_entry(self, index_name: str, entry_id: Any) -> pymongo.results.DeleteResult:
+        """
+        Deletes DB entry from the collection by given index name and entry_id. Raises exception if number of deleted
+        entries is not equal to 1
+
+        :param str index_name: index name used to filter data in DB collection
+        :param Any entry_id: ID of the DB entry to be deleted from the collection
+        :raises ValueError: if number of deleted entries in DB collection is greater than 1, raises ValueError
+        :return result: pymongo.results.DeleteResult
+        """
+        q_filter = {index_name: entry_id}
+        result = self.collection.delete_one(filter=q_filter)
+        if result.deleted_count > 1:
+            raise ValueError(
+                f"Error while deleting entry {entry_id} by index {index_name} from the collection {self.collection}, "
+                f"deleted: {result.deleted_count} entries, expected: 0 or 1!"
+            )
+        return result
+
+    def find_one_and_delete(self, data: Dict[str, Any]) -> Dict:
+        """
+        Finds a single document and deletes it, returning the document
+
+        :param Dict[str, Any] data: document to be deleted from DB
+        :return Dict: document
+        """
+        return self.collection.find_one_and_delete(data)
